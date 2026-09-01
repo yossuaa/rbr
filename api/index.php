c969589b451f273feaf71b8e54c8c329a41c5a67
yossuaa <yhssua@gmail.com>
2026-09-01 13:34:52 +0800

    fix laravel 12 vercel entrypoint

--- api/index.php
+++ api/index.php
@@ -1,13 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$request = Illuminate\Http\Request::capture();
-
-$response = $app->handleRequest($request);
-
-$response->send();
-
-$app->terminate($request, $response);
+$app->handleRequest(Request::capture());
